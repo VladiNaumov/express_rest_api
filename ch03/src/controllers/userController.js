03a2063f74538de915778bad946f08dd31048e91
NaumDeveloper <vov4.games@yandex.ru>
2023-11-30 18:48:06 +0200

    corrected

--- ch03/src/controllers/userController.js
+++ ch03/src/controllers/userController.js
@@ -1,9 +1,24 @@
 const User = require("../models/user");
 
+/*
+Request : From Client to Server
+Response: From Server to Client
+Server: Receive Request and Send Response
+Client: Send Request and Receive Response
+*/
+
 exports.addUser = function (request, response){
-    const user = new User('naumov', 32)
-    user.save()
-    response.send("пользователь добавлен" );
+    try{
+        console.log(request)
+        const name = request.name
+        const age = request.age
+        const user = new User(name, age)
+        user.save()
+        response.send("пользователь добавлен" );
+    }catch (e){
+        console.log(e)
+    }
+
 
 };
 
