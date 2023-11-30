ce0fd63da1249ca8841c612aa0d0397ebb2cf5c3
NaumDeveloper <vov4.games@yandex.ru>
2023-11-30 17:54:49 +0200

    corrected

--- ch03/src/controllers/homeController.js
+++ ch03/src/controllers/homeController.js
@@ -1,6 +1,13 @@
+const path = require("path");
+
+
 exports.index = function (request, response) {
-    response.send("Главная страница");
+    //response.send ("index")
+    response.sendFile(path.resolve("./public/index.html"))
 };
 exports.about = function (request, response) {
-    response.send("О сайте");
+    //response.send ("About")
+    response.sendFile(path.resolve("./public/about.html"))
+
 };
+
